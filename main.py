1015e547008f92e60d49fa41d7e51941c25a11bb
Peter Harpending <pharpend2@gmail.com>
2013-03-28 16:34:22 -0600

    I untabified the code - I changed the <tab> character (\t) to four spaces (\s\s\s\s). This ensures consistent line length. It also puts my code in compliance with PEP 8.

--- main.py
+++ main.py
@@ -34,139 +34,139 @@ global general_pool
 global individual_groups
 
 def intermittent_process():
-	'''
-	This is the code that runs between the groups getting split up, and then put
-	back together. The idea is, people can put whatever stat-collecting code in
-	here.
+    '''
+    This is the code that runs between the groups getting split up, and then put
+    back together. The idea is, people can put whatever stat-collecting code in
+    here.
 
-	Currently, it just has each group mate.
-	'''
+    Currently, it just has each group mate.
+    '''
 
-	global individual_groups
+    global individual_groups
 
-	# Each group should mate
-	[group.mate_pool() for group in individual_groups]
+    # Each group should mate
+    [group.mate_pool() for group in individual_groups]
 # end of intermittent_process()
-	
+    
 def main():
-	'''
-	This parses command line arguments, then runs sim().
-	'''
-	
-	global number_of_people
-	global number_of_groups
-	global number_of_runs
-	global mean
-	global standard_deviation
-	global heritability
+    '''
+    This parses command line arguments, then runs sim().
+    '''
+    
+    global number_of_people
+    global number_of_groups
+    global number_of_runs
+    global mean
+    global standard_deviation
+    global heritability
 
-	try:
-		number_of_people = int(sys.argv[1])
-		number_of_groups = int(sys.argv[2])
-		number_of_runs = int(sys.argv[3])
-		mean = float(sys.argv[4])
-		standard_deviation = float((sys.argv[5]))
-		heritability = float(sys.argv[6])
-		
-		sim()
-		
-	# If the arguments are invalid or don't exist
-	except Exception as e:
-		print(traceback.format_exc())
-		
-		help_message = 'Usage: python main.py n g r m s h\n'
-		help_message += 'n = sample size\n'
-		help_message += 'g = number of groups\n'
-		help_message += 'r = number of runs\n'
-		help_message += 'm = mean\n'
-		help_message += 's = standard deviation\n'
-		help_message += 'h = heritability\n'
-		
-		print(help_message)
-		exit()
+    try:
+        number_of_people = int(sys.argv[1])
+        number_of_groups = int(sys.argv[2])
+        number_of_runs = int(sys.argv[3])
+        mean = float(sys.argv[4])
+        standard_deviation = float((sys.argv[5]))
+        heritability = float(sys.argv[6])
+        
+        sim()
+        
+    # If the arguments are invalid or don't exist
+    except Exception as e:
+        print(traceback.format_exc())
+        
+        help_message = 'Usage: python main.py n g r m s h\n'
+        help_message += 'n = sample size\n'
+        help_message += 'g = number of groups\n'
+        help_message += 'r = number of runs\n'
+        help_message += 'm = mean\n'
+        help_message += 's = standard deviation\n'
+        help_message += 'h = heritability\n'
+        
+        print(help_message)
+        exit()
 # end of main()
 
 def make_pool():
-	'''
-	This function makes, and returns a list of Person objects.
-	'''
+    '''
+    This function makes, and returns a list of Person objects.
+    '''
 
-	global number_of_people
-	global number_of_groups
-	global number_of_runs
-	global mean
-	global standard_deviation
-	global heritability
-	global genic_population
-	global env_population
+    global number_of_people
+    global number_of_groups
+    global number_of_runs
+    global mean
+    global standard_deviation
+    global heritability
+    global genic_population
+    global env_population
 
-	# Compute the genic mean; easy.
-	genic_mean = mean*heritability
-	
-	# The genic std is a bit harder
-	variance = standard_deviation**2
-	genic_variance = heritability*variance
-	genic_standard_deviation = genic_variance**0.5
+    # Compute the genic mean; easy.
+    genic_mean = mean*heritability
+    
+    # The genic std is a bit harder
+    variance = standard_deviation**2
+    genic_variance = heritability*variance
+    genic_standard_deviation = genic_variance**0.5
 
-	# The environmential variables are whatever is left
-	env_mean = mean - genic_mean
-	env_variance = variance - genic_variance
-	env_standard_deviation = env_variance**0.5
+    # The environmential variables are whatever is left
+    env_mean = mean - genic_mean
+    env_variance = variance - genic_variance
+    env_standard_deviation = env_variance**0.5
 
-	# First, make the population according to a normal distribution
-	genic_population = np.random.normal(genic_mean,
-										genic_standard_deviation,
-										number_of_people)
-	env_population = np.random.normal(env_mean,
-									  env_standard_deviation,
-									  number_of_people)
+    # First, make the population according to a normal distribution
+    genic_population = np.random.normal(genic_mean,
+                                        genic_standard_deviation,
+                                        number_of_people)
+    env_population = np.random.normal(env_mean,
+                                      env_standard_deviation,
+                                      number_of_people)
 
-	return pool.Pool(genic_population, env_population)
+    return pool.Pool(genic_population, env_population)
 # end of make_pool()
-	
+    
 
 def sim():
-	'''
-	This function actually runs the simulation.
+    '''
+    This function actually runs the simulation.
 
-	So, assuming the heritability is h, and the trait we are measuring is IQ,
-	the genic mean for IQ is h*(mean_iq).
+    So, assuming the heritability is h, and the trait we are measuring is IQ,
+    the genic mean for IQ is h*(mean_iq).
 
-	The variance for IQ would be standard_deviation**2. So, the genic standard
-	deviation is sqrt(standard_deviation**2)*.8)
-	'''
-	
-	# Global variables
-	global number_of_people
-	global number_of_groups
-	global number_of_runs
-	global mean
-	global standard_deviation
-	global heritability
-	global general_pool
-	global individual_groups
+    The variance for IQ would be standard_deviation**2. So, the genic standard
+    deviation is sqrt(standard_deviation**2)*.8)
+    '''
+    
+    # Global variables
+    global number_of_people
+    global number_of_groups
+    global number_of_runs
+    global mean
+    global standard_deviation
+    global heritability
+    global general_pool
+    global individual_groups
 
-	# Make the initial people objects
-	general_pool = make_pool()
-	
-	# Run the simulation
-	for i in range(number_of_runs):
-		# Split up the groups
-		individual_groups = general_pool.partition(number_of_groups)
-		
-		# Run whatever is in intermittent_process()
-		intermittent_process()
-		
-		general_pool.drain()
-		# Conglomerate the pools
-		for group in individual_groups:
-			general_pool += group
-			
-	# end of for
-		
+    # Make the initial people objects
+    general_pool = make_pool()
+    
+    # Run the simulation
+    for i in range(number_of_runs):
+        # Split up the groups
+        individual_groups = general_pool.partition(number_of_groups)
+        
+        # Run whatever is in intermittent_process()
+        intermittent_process()
+        
+        general_pool.drain()
+        # Conglomerate the pools
+        for group in individual_groups:
+            general_pool += group
+            
+    # end of for
+        
 # end of make()
-	
-	
+    
+    
 
 if __name__ == '__main__':
-	main()
+    main()
--- pool.py
+++ pool.py
@@ -9,160 +9,160 @@ import random as r
 import numpy as np
 
 class Pool:
-	'''
-	This is a class for a group of people for the genetic casting simulator.
-	'''
+    '''
+    This is a class for a group of people for the genetic casting simulator.
+    '''
 
-	def __add__(self, other):
-		'''
-		Add two Pools together. Overloads the + operator
-		'''
-		
-		all_genic = list(self.genic_population) + list(other.genic_population)
-		all_env = list(self.env_population) + list(other.env_population)
-		
-		all_genic = np.array(all_genic)
-		all_env = np.array(all_env)
+    def __add__(self, other):
+        '''
+        Add two Pools together. Overloads the + operator
+        '''
+        
+        all_genic = list(self.genic_population) + list(other.genic_population)
+        all_env = list(self.env_population) + list(other.env_population)
+        
+        all_genic = np.array(all_genic)
+        all_env = np.array(all_env)
 
-		return Pool(all_genic, all_env)
-	# end of + overloading
+        return Pool(all_genic, all_env)
+    # end of + overloading
 
-	def __init__(self,
-				 genic_population: np.array,
-				 env_population: np.array):
-		'''
-		This sets instance variables equal to arguments, then calls make().
-		'''
+    def __init__(self,
+                 genic_population: np.array,
+                 env_population: np.array):
+        '''
+        This sets instance variables equal to arguments, then calls make().
+        '''
 
-		self.genic_population = genic_population
-		self.env_population = env_population
+        self.genic_population = genic_population
+        self.env_population = env_population
 
-		self.make()
+        self.make()
 
-	# end of constructor
+    # end of constructor
 
-	def drain(self):
-		'''
-		Empties the pool.
-		'''
+    def drain(self):
+        '''
+        Empties the pool.
+        '''
 
-		self.genic_population = np.array([])
-		self.env_population = np.array([])
-	# end of drain()
+        self.genic_population = np.array([])
+        self.env_population = np.array([])
+    # end of drain()
 
-	def make(self):
-		'''
-		This makes the Pool.
-		'''
-		
-		'''
-		This list comprehension is admittedly a bit dense. Basically, it makes a
-		tuple associating each genic value with an environmental value. Then, it
-		makes a list of those tuples, and convert it into a numpy array.
-		'''
-		self.persons = [(g, e)
-						for g, e in zip(self.genic_population,
-										self.env_population)]
-		self.persons.sort()
-		self.persons = np.array(self.persons)
-		
-	# end of make()
+    def make(self):
+        '''
+        This makes the Pool.
+        '''
+        
+        '''
+        This list comprehension is admittedly a bit dense. Basically, it makes a
+        tuple associating each genic value with an environmental value. Then, it
+        makes a list of those tuples, and convert it into a numpy array.
+        '''
+        self.persons = [(g, e)
+                        for g, e in zip(self.genic_population,
+                                        self.env_population)]
+        self.persons.sort()
+        self.persons = np.array(self.persons)
+        
+    # end of make()
 
-	def mate_pool(self):
-		'''
-		This has everyone in the Pool to mate. It returns another Pool.
-		'''
+    def mate_pool(self):
+        '''
+        This has everyone in the Pool to mate. It returns another Pool.
+        '''
 
-		'''
-		Here is the process: first, it shuffles the pool. Then, it splits the
-		pool into two equal parts, each part being one sex. Then, it averages
-		the genic values of the two mates. It produces two children per pair.
-		'''
+        '''
+        Here is the process: first, it shuffles the pool. Then, it splits the
+        pool into two equal parts, each part being one sex. Then, it averages
+        the genic values of the two mates. It produces two children per pair.
+        '''
 
-		# Environmental normal values
-		env_mean = np.mean(self.env_population)
-		env_std = np.std(self.env_population)
-		
-		# Shuffle the pool
-		people_ls = list(self.persons)
-		r.shuffle(people_ls)
-		people_ls = np.array(people_ls)
-#		print(people_ls)
+        # Environmental normal values
+        env_mean = np.mean(self.env_population)
+        env_std = np.std(self.env_population)
+        
+        # Shuffle the pool
+        people_ls = list(self.persons)
+        r.shuffle(people_ls)
+        people_ls = np.array(people_ls)
+#       print(people_ls)
 
-		# Split it up
-		people_ls1, people_ls2 = np.split(people_ls, 2)
-		n_pairs = len(people_ls1)
+        # Split it up
+        people_ls1, people_ls2 = np.split(people_ls, 2)
+        n_pairs = len(people_ls1)
 
-		# Mate 
-#		children = []
-		all_genic_vals = []
-		all_env_vals = []
-		for pair_num in range(n_pairs):
-			person1 = people_ls1[pair_num]
-			person2 = people_ls2[pair_num]
-#			print(person1)
+        # Mate 
+#       children = []
+        all_genic_vals = []
+        all_env_vals = []
+        for pair_num in range(n_pairs):
+            person1 = people_ls1[pair_num]
+            person2 = people_ls2[pair_num]
+#           print(person1)
 
-			person1_genic = person1[0]
-			person2_genic = person2[0]
-			
-			genic_vals = [person1_genic, person2_genic]
-			genic_mean = np.mean(genic_vals)
-			genic_std = np.std(genic_vals)
-			
-			child1_genic_val = r.gauss(genic_mean, genic_std)
-			child2_genic_val = r.gauss(genic_mean, genic_std)
-#			print(child1_genic_val)
+            person1_genic = person1[0]
+            person2_genic = person2[0]
+            
+            genic_vals = [person1_genic, person2_genic]
+            genic_mean = np.mean(genic_vals)
+            genic_std = np.std(genic_vals)
+            
+            child1_genic_val = r.gauss(genic_mean, genic_std)
+            child2_genic_val = r.gauss(genic_mean, genic_std)
+#           print(child1_genic_val)
 
-			child1_env_val = r.gauss(env_mean, env_std)
-			child2_env_val = r.gauss(env_mean, env_std)
+            child1_env_val = r.gauss(env_mean, env_std)
+            child2_env_val = r.gauss(env_mean, env_std)
 
-			all_genic_vals.append(child1_genic_val)
-			all_genic_vals.append(child2_genic_val)
-			all_env_vals.append(child1_env_val)
-			all_env_vals.append(child2_env_val)
-			'''
-			child1 = [child1_genic_val, child1_env_val]
-			child2 = [child2_genic_val, child2_env_val]
-#			print(child1)
+            all_genic_vals.append(child1_genic_val)
+            all_genic_vals.append(child2_genic_val)
+            all_env_vals.append(child1_env_val)
+            all_env_vals.append(child2_env_val)
+            '''
+            child1 = [child1_genic_val, child1_env_val]
+            child2 = [child2_genic_val, child2_env_val]
+#           print(child1)
 
-			children.append(child1)
-			children.append(child2)
-			'''
-		# end for
+            children.append(child1)
+            children.append(child2)
+            '''
+        # end for
 
-		all_genic_vals = np.array(all_genic_vals)
-		all_env_vals = np.array(all_env_vals)
-		
-		return Pool(all_genic_vals, all_env_vals)
-	# end of mate()
-		
-	def partition(self,
-				  number_of_groups: int = 2):
-		'''
-		Partitions the pool into equal sized groups.
-		'''
-		
-		'''
-		This code may be a bit difficult to read, but here goes. It partitions
-		the pool into a set number of groups. Then, it constructs Pool objects
-		out of those groups, and returns a list of those.
-		'''
-		
-		# Alright, so, this partitions the pool
-		groups_array = np.split(self.persons, number_of_groups)
-#		print(groups_array)
+        all_genic_vals = np.array(all_genic_vals)
+        all_env_vals = np.array(all_env_vals)
+        
+        return Pool(all_genic_vals, all_env_vals)
+    # end of mate()
+        
+    def partition(self,
+                  number_of_groups: int = 2):
+        '''
+        Partitions the pool into equal sized groups.
+        '''
+        
+        '''
+        This code may be a bit difficult to read, but here goes. It partitions
+        the pool into a set number of groups. Then, it constructs Pool objects
+        out of those groups, and returns a list of those.
+        '''
+        
+        # Alright, so, this partitions the pool
+        groups_array = np.split(self.persons, number_of_groups)
+#       print(groups_array)
 
-		groups_all = []
-		# For each group
-		for group in groups_array:
-			# Get the genic values
-			genic = [vals[0] for vals in group]
-			env = [vals[1] for vals in group]
-			new_pool = Pool(genic, env)
-			groups_all.append(new_pool)
+        groups_all = []
+        # For each group
+        for group in groups_array:
+            # Get the genic values
+            genic = [vals[0] for vals in group]
+            env = [vals[1] for vals in group]
+            new_pool = Pool(genic, env)
+            groups_all.append(new_pool)
 
-		return groups_all
-	# end of partition()
-		
+        return groups_all
+    # end of partition()
+        
 # end of class
-	
+    
