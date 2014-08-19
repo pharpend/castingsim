ff36f40723e67638bc0c9701371ab8286103ce8d
Peter Harpending <pharpend2@gmail.com>
2014-08-19 12:06:22 -0600

    Add new skel and .gitignore.

---
+++ skel/Skel.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module       : Data.Semiring.Tropical.Skel
+-- Description  : A module for Tropical whatever
+-- Copyright    : 2014, Peter Harpending
+-- License      : BSD3
+-- Maintainer   : Peter Harpending <pharpend2@gmail.com>
+-- Stability    : experimental
+-- Portability  : archlinux
+
+module Data.Semiring.Tropical.Skel where
